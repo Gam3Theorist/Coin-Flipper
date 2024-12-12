d5838747e90d7e7abbba453f4bb355e170b688ab
adam.ward <adam.ward@MU-C1-02.mullen.pvt.k12.co.us>
2024-12-12 15:10:25 -0700

    initial commit

---
+++ Coin Flipper/src/coinFlipper.java
@@ -0,0 +1,34 @@
+
+public class coinFlipper
+	{
+
+		public static void main(String[] args)
+			{
+				int randomNumber = (int) (Math.random()*2);
+
+			
+			if (randomNumber==0)
+				{
+					System.out.println("You flipped heads!");
+				}
+			
+			else
+				{
+					System.out.println("You flipped tails!");
+				}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			}
+
+	}
